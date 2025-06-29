b3d53c0f0915087e6a48372c9184e0a7aa5e2bb3
Daniel Dias <danielmartinhodias@gmail.com>
2025-06-29 21:28:34 +0100

    try3

--- frontpage.py
+++ frontpage.py
@@ -42,63 +42,64 @@ with st.form("my_form"):
 if submitted:
   if not nome or not cc or not inicio_ausencia or not fim_ausencia or not agree:
     st.error("Please fill in all required fields (Name and Email).")
-  try:
-    success_string=f"Eu, Daniel Martinho Ferreira Dias, portador da cédula profissional 63783, venho por este meio declarar que {nome}, portador do CC/BI {cc}, encontra-se doente, com incapacidade laboral desde {inicio_ausencia} e previsível até {fim_ausencia}. Por ser verdade e me ter sido pedido, emito a presente declaração que dato e assino"
-    st.success(success_string)
-    st.success("Enviado e-mail para o médico avaliar e validar.")
+  else:
+    try:
+      success_string=f"Eu, Daniel Martinho Ferreira Dias, portador da cédula profissional 63783, venho por este meio declarar que {nome}, portador do CC/BI {cc}, encontra-se doente, com incapacidade laboral desde {inicio_ausencia} e previsível até {fim_ausencia}. Por ser verdade e me ter sido pedido, emito a presente declaração que dato e assino"
+      st.success(success_string)
+      st.success("Enviado e-mail para o médico avaliar e validar.")
 
-    doc = Document("./templates/declaracao_ausencia.docx")
-    paragraph=doc.add_paragraph()
-    paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-    run = paragraph.add_run("Declaração de Doença")
-    run.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-    run.bold = True                    # Make text bold
-    run.font.name = 'Arial'           # Set font name
-    run.font.size = Pt(16)            # Set font size (e.g., 14 pt)
-    
-    paragraph2=doc.add_paragraph(success_string)
-    paragraph2.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+      doc = Document("./templates/declaracao_ausencia.docx")
+      paragraph=doc.add_paragraph()
+      paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+      run = paragraph.add_run("Declaração de Doença")
+      run.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+      run.bold = True                    # Make text bold
+      run.font.name = 'Arial'           # Set font name
+      run.font.size = Pt(16)            # Set font size (e.g., 14 pt)
+      
+      paragraph2=doc.add_paragraph(success_string)
+      paragraph2.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
 
-    doc.save(f"./issued/declaracoes/dec_doenca{nome}.docx")
+      doc.save(f"./issued/declaracoes/dec_doenca{nome}.docx")
 
-    convert(f"./issued/declaracoes/dec_doenca{nome}.docx")
+      convert(f"./issued/declaracoes/dec_doenca{nome}.docx")
 
-      # Email credentials
-    sender_email = "danielmartinhodias@gmail.com"
-    receiver_email = "danielmartinhodias@gmail.com"
-    app_password = "dsxxuvwybaevopdu"
+        # Email credentials
+      sender_email = "danielmartinhodias@gmail.com"
+      receiver_email = "danielmartinhodias@gmail.com"
+      app_password = "dsxxuvwybaevopdu"
 
-    # Email content
-    subject = f"Dec_doença_{nome}"
-    body = f"Para editar, validar e assinar: {success_string}"
+      # Email content
+      subject = f"Dec_doença_{nome}"
+      body = f"Para editar, validar e assinar: {success_string}"
 
-    # Create the email
-    msg = MIMEMultipart()
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
-    msg["Subject"] = subject
+      # Create the email
+      msg = MIMEMultipart()
+      msg["From"] = sender_email
+      msg["To"] = receiver_email
+      msg["Subject"] = subject
 
-    mensagem = f'Para validação de pedido de declaração de ausência de {nome}, submetida em {timestamp}'
-    msg.attach(MIMEText(mensagem, 'plain'))
+      mensagem = f'Para validação de pedido de declaração de ausência de {nome}, submetida em {timestamp}'
+      msg.attach(MIMEText(mensagem, 'plain'))
 
-    anexos = [f"./issued/declaracoes/dec_doenca{nome}.docx", f"./issued/declaracoes/dec_doenca{nome}.pdf"]
+      anexos = [f"./issued/declaracoes/dec_doenca{nome}.docx", f"./issued/declaracoes/dec_doenca{nome}.pdf"]
 
-    # Anexar arquivos
-    for ficheiro in anexos:
-        with open(ficheiro, 'rb') as f:
-            parte = MIMEBase('application', 'octet-stream')
-            parte.set_payload(f.read())
-            encoders.encode_base64(parte)
-            parte.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(ficheiro)}"')
-            msg.attach(parte)
+      # Anexar arquivos
+      for ficheiro in anexos:
+          with open(ficheiro, 'rb') as f:
+              parte = MIMEBase('application', 'octet-stream')
+              parte.set_payload(f.read())
+              encoders.encode_base64(parte)
+              parte.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(ficheiro)}"')
+              msg.attach(parte)
 
-    # Enviar e-mail
+      # Enviar e-mail
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(sender_email, app_password)
-        smtp.send_message(msg)
+      with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+          smtp.login(sender_email, app_password)
+          smtp.send_message(msg)
 
-    print("Email enviado com sucesso para o médico para validação. Aguarde contacto e resposta.")
+      print("Email enviado com sucesso para o médico para validação. Aguarde contacto e resposta.")
 
-  except:
-    pass
+    except:
+      pass
